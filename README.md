a26e2c2bdb0cccc80db4d62073d9915bbbaafa19
Leo <losoviz@gmail.com>
2019-10-28 18:07:14 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# Translate Directive
+
+<!--
+[![Latest Version on Packagist][ico-version]][link-packagist]
+[![Software License][ico-license]](LICENSE.md)
+[![Build Status][ico-travis]][link-travis]
+[![Coverage Status][ico-scrutinizer]][link-scrutinizer]
+[![Quality Score][ico-code-quality]][link-code-quality]
+[![Total Downloads][ico-downloads]][link-downloads]
+-->
+
+Directive <translate> to translate content using different provider APIs
+
+## Install
+
+Via Composer
+
+``` bash
+$ composer require getpop/translate-directive dev-master
+```
+
+**Note:** Your `composer.json` file must have the configuration below to accept minimum stability `"dev"` (there are no releases for PoP yet, and the code is installed directly from the `master` branch):
+
+```javascript
+{
+    ...
+    "minimum-stability": "dev",
+    "prefer-stable": true,
+    ...
+}
+```
+
+## Usage
+
+Extend from class `AbstractTranslateDirectiveResolver` to implement the translation directive using a specific API provider.
+
+## Change log
+
+Please see [CHANGELOG](CHANGELOG.md) for more information on what has changed recently.
+
+## Testing
+
+``` bash
+$ composer test
+```
+
+## Contributing
+
+Please see [CONTRIBUTING](CONTRIBUTING.md) and [CODE_OF_CONDUCT](CODE_OF_CONDUCT.md) for details.
+
+## Security
+
+If you discover any security related issues, please email leo@getpop.org instead of using the issue tracker.
+
+## Credits
+
+- [Leonardo Losoviz][link-author]
+- [All Contributors][link-contributors]
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
+
+[ico-version]: https://img.shields.io/packagist/v/getpop/translate-directive.svg?style=flat-square
+[ico-license]: https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat-square
+[ico-travis]: https://img.shields.io/travis/getpop/translate-directive/master.svg?style=flat-square
+[ico-scrutinizer]: https://img.shields.io/scrutinizer/coverage/g/getpop/translate-directive.svg?style=flat-square
+[ico-code-quality]: https://img.shields.io/scrutinizer/g/getpop/translate-directive.svg?style=flat-square
+[ico-downloads]: https://img.shields.io/packagist/dt/getpop/translate-directive.svg?style=flat-square
+
+[link-packagist]: https://packagist.org/packages/getpop/translate-directive
+[link-travis]: https://travis-ci.org/getpop/translate-directive
+[link-scrutinizer]: https://scrutinizer-ci.com/g/getpop/translate-directive/code-structure
+[link-code-quality]: https://scrutinizer-ci.com/g/getpop/translate-directive
+[link-downloads]: https://packagist.org/packages/getpop/translate-directive
+[link-author]: https://github.com/leoloso
+[link-contributors]: ../../contributors
+
---
+++ src/DirectiveResolvers/AbstractTranslateDirectiveResolver.php
@@ -0,0 +1,83 @@
+<?php
+namespace PoP\TranslateDirective\DirectiveResolvers;
+use PoP\ComponentModel\GeneralUtils;
+use PoP\ComponentModel\FieldResolvers\PipelinePositions;
+use PoP\ComponentModel\FieldResolvers\FieldResolverInterface;
+use PoP\ComponentModel\Facades\Schema\FieldQueryInterpreterFacade;
+use PoP\ComponentModel\DirectiveResolvers\AbstractDirectiveResolver;
+use PoP\GuzzleHelpers\GuzzleHelpers;
+
+abstract class AbstractTranslateDirectiveResolver extends AbstractDirectiveResolver
+{
+    /**
+     * This directive must be executed after ResolveAndMerge, and modify values directly on the returned DB items
+     *
+     * @return void
+     */
+    public function getPipelinePosition(): string
+    {
+        return PipelinePositions::BACK;
+    }
+
+    public function resolveDirective(FieldResolverInterface $fieldResolver, array &$resultIDItems, array &$idsDataFields, array &$dbItems, array &$dbErrors, array &$dbWarnings, array &$schemaErrors, array &$schemaWarnings, array &$schemaDeprecations)
+    {
+        // Replace all the strings with their translation
+        $provider = 'google';
+        $sourceLang = 'en';
+        $targetLang = 'es';
+        $contents = [];
+        // Keep track of which translation must be placed where
+        $translationPositions = [];
+        $counter = 0;
+        if ($provider) {
+            // Collect all the pieces of text to translate
+            $fieldQueryInterpreter = FieldQueryInterpreterFacade::getInstance();
+            $fieldOutputKeyCache = [];
+            foreach ($idsDataFields as $id => $dataFields) {
+                foreach ($dataFields['direct'] as $field) {
+                    // Get the fieldOutputKey from the cache, or calculate it
+                    if (is_null($fieldOutputKeyCache[$field])) {
+                        $fieldOutputKeyCache[$field] = $fieldQueryInterpreter->getFieldOutputKey($field);
+                    }
+                    $fieldOutputKey = $fieldOutputKeyCache[$field];
+                    // Add the text to be translated, and keep the position from where it will be retrieved
+                    $contents[] = $dbItems[$id][$fieldOutputKey];
+                    $translationPositions[$id][$fieldOutputKey] = $counter;
+                    $counter++;
+                }
+            }
+        }
+        if ($contents) {
+            // Execute the endpoint
+            if ($endpointURL = $this->getEndpoint($provider, $sourceLang, $targetLang, $contents)) {
+                $query = $this->getQuery($provider, $sourceLang, $targetLang, $contents);
+                $response = GuzzleHelpers::requestJSON($endpointURL, $query);
+                if (GeneralUtils::isError($response)) {
+                    return $response;
+                }
+                $translations = $this->extractTranslationsFromResponse($provider, $response);
+                // Iterate through the translations, and replace the original content in the dbItems object
+                foreach ($idsDataFields as $id => $dataFields) {
+                    foreach ($dataFields['direct'] as $field) {
+                        $fieldOutputKey = $fieldOutputKeyCache[$field];
+                        // Add the text to be translated, and keep the position from where it will be retrieved
+                        $position = $translationPositions[$id][$fieldOutputKey];
+                        $dbItems[$id][$fieldOutputKey] = $translations[$position];
+                    }
+                }
+            }
+        }
+    }
+
+    protected abstract function getEndpoint(string $provider, string $sourceLang, string $targetLang, array $contents);
+
+    protected function getQuery(string $provider, string $sourceLang, string $targetLang, array $contents)
+    {
+        return null;
+    }
+
+    protected function extractTranslationsFromResponse(string $provider, array $response): array
+    {
+        return $response;
+    }
+}
