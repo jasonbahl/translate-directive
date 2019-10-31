ef1d240167b3890229d17cab1c97e997260d01e8
Leo <losoviz@gmail.com>
2019-10-31 17:36:14 +0800

    Extract "provider" directiveArg

--- src/DirectiveResolvers/AbstractTranslateDirectiveResolver.php
+++ src/DirectiveResolvers/AbstractTranslateDirectiveResolver.php
@@ -27,103 +27,105 @@ abstract class AbstractTranslateDirectiveResolver extends AbstractSchemaDirectiv
         return PipelinePositions::BACK;
     }
 
-    /**
-     * If not passing a provider in the directive, use the default one instead
+        /**
+     * The name of the API's provider
      *
-     * @return void
+     * @return array
      */
-    public function getDefaultProvider(): ?string
-    {
-        return null;
-    }
+    public abstract function getProvidersToResolve(): array;
 
     public function resolveDirective(FieldResolverInterface $fieldResolver, array &$resultIDItems, array &$idsDataFields, array &$dbItems, array &$dbErrors, array &$dbWarnings, array &$schemaErrors, array &$schemaWarnings, array &$schemaDeprecations)
     {
-        // Replace all the strings with their translation
-        $provider = 'google';
-        if ($provider) {
-            $fieldQueryInterpreter = FieldQueryInterpreterFacade::getInstance();
-            $translationAPI = TranslationAPIFacade::getInstance();
-            // Make sure that there is an endpoint
-            $endpointURL = $this->getEndpoint($provider);
-            if (!$endpointURL) {
-                // Give an error message for all failed fields
-                $failureMessage = sprintf(
-                    $translationAPI->__('Provider \'%s\' doesn\'t have an endpoint URL configured, so it can\'t proceed to do the translation', 'component-model'),
-                    $provider
-                );
-                $this->processFailure($failureMessage, [], $idsDataFields, $schemaErrors, $schemaWarnings);
-                // Nothing else to do
-                return;
+        $fieldQueryInterpreter = FieldQueryInterpreterFacade::getInstance();
+        $translationAPI = TranslationAPIFacade::getInstance();
+
+        // Retrieve the provider from the directiveArgs. All results must have the same provider, that's why it's taken from schema, and not from resultItem (as the from/to langs)
+        list(
+            $directive,
+            $directiveName,
+            $directiveArgs
+        ) = $fieldQueryInterpreter->extractDirectiveArgumentsForSchema($fieldResolver, $this->directive);
+        $provider = $directiveArgs['provider'];
+        // Make sure that there is an endpoint
+        $endpointURL = $this->getEndpoint($provider);
+        if (!$endpointURL) {
+            // Give an error message for all failed fields
+            $failureMessage = sprintf(
+                $translationAPI->__('Provider \'%s\' doesn\'t have an endpoint URL configured, so it can\'t proceed to do the translation', 'component-model'),
+                $provider
+            );
+            $this->processFailure($failureMessage, [], $idsDataFields, $schemaErrors, $schemaWarnings);
+            // Nothing else to do
+            return;
+        }
+        // Keep all the translations to be made by pairs of to/from language
+        $contentsBySourceTargetLang = [];
+        // Keep track of which translation must be placed where
+        $translationPositions = [];
+        // Collect all the pieces of text to translate
+        $fieldOutputKeyCache = [];
+        $counters = [];
+        foreach ($idsDataFields as $id => $dataFields) {
+            // Extract the from/to language from the params. Each pair of from/to languages can be set on a result-by-result basis,
+            // that's why it's taken from resultItem and not from schema (as the provider is)
+            $resultItem = $resultIDItems[$id];
+            list(
+                $resultItemValidDirective,
+                $resultItemDirectiveName,
+                $resultItemDirectiveArgs
+            ) = $this->dissectAndValidateDirectiveForResultItem($fieldResolver, $resultItem, $this->directive, $dbErrors, $dbWarnings);
+            // Check that the directive is valid. If it is not, $dbErrors will have the error already added
+            if (is_null($resultItemValidDirective)) {
+                continue;
             }
-            // Keep all the translations to be made by pairs of to/from language
-            $contentsBySourceTargetLang = [];
-            // Keep track of which translation must be placed where
-            $translationPositions = [];
-            // Collect all the pieces of text to translate
-            $fieldOutputKeyCache = [];
-            $counters = [];
-            foreach ($idsDataFields as $id => $dataFields) {
-                // Extract the from/to language from the params
-                $resultItem = $resultIDItems[$id];
-                list(
-                    $resultItemValidDirective,
-                    $resultItemDirectiveName,
-                    $resultItemDirectiveArgs
-                ) = $this->dissectAndValidateDirectiveForResultItem($fieldResolver, $resultItem, $this->directive, $dbErrors, $dbWarnings);
-                // Check that the directive is valid. If it is not, $dbErrors will have the error already added
-                if (is_null($resultItemValidDirective)) {
-                    continue;
-                }
-                $sourceLang = $resultItemDirectiveArgs['from'];
-                $targetLang = $resultItemDirectiveArgs['to'];
-                if (!isset($counters[$sourceLang][$targetLang])) {
-                    $counters[$sourceLang][$targetLang] = 0;
-                }
-                foreach ($dataFields['direct'] as $field) {
-                    // Get the fieldOutputKey from the cache, or calculate it
-                    if (is_null($fieldOutputKeyCache[$field])) {
-                        $fieldOutputKeyCache[$field] = $fieldQueryInterpreter->getFieldOutputKey($field);
-                    }
-                    $fieldOutputKey = $fieldOutputKeyCache[$field];
-                    // Add the text to be translated, and keep the position from where it will be retrieved
-                    $contentsBySourceTargetLang[$sourceLang][$targetLang][] = $dbItems[$id][$fieldOutputKey];
-                    $translationPositions[$sourceLang][$targetLang][$id][$fieldOutputKey] = $counters[$sourceLang][$targetLang];
-                    $counters[$sourceLang][$targetLang]++;
-                }
+            $sourceLang = $resultItemDirectiveArgs['from'];
+            $targetLang = $resultItemDirectiveArgs['to'];
+            if (!isset($counters[$sourceLang][$targetLang])) {
+                $counters[$sourceLang][$targetLang] = 0;
             }
-            // Translate all the contents for each pair of from/to languages
-            foreach ($contentsBySourceTargetLang as $sourceLang => $targetLangContents) {
-                foreach ($targetLangContents as $targetLang => $contents) {
-                    // Get the query to send in the request, and execute against the endpoint
-                    $query = $this->getQuery($provider, $sourceLang, $targetLang, $contents);
-                    $response = GuzzleHelpers::requestJSON($endpointURL, $query);
-                    // If the request failed, show an error and do nothing else
-                    if (GeneralUtils::isError($response)) {
-                        $error = $response;
-                        $failureMessage = sprintf(
-                            $translationAPI->__('There was an error requesting data from the Provider API: %s', 'component-model'),
-                            $error->getErrorMessage()
-                        );
-                        $this->processFailure($failureMessage, [], $idsDataFields, $schemaErrors, $schemaWarnings);
-                        return;
-                    }
-                    $response = (array)$response;
-                    // Validate if the response is the translation, or some error from the service provider
-                    if ($errorMessage = $this->getErrorMessageFromResponse($provider, $response)) {
-                        $failureMessage = sprintf(
-                            $translationAPI->__('There was an error processing the response from the Provider API: %s', 'component-model'),
-                            $errorMessage
-                        );
-                        $this->processFailure($failureMessage, [], $idsDataFields, $schemaErrors, $schemaWarnings);
-                        return;
-                    }
-                    $translations = $this->extractTranslationsFromResponse($provider, $response);
-                    // Iterate through the translations, and replace the original content in the dbItems object
-                    foreach ($translationPositions[$sourceLang][$targetLang] as $id => $fieldOutputKeyPosition) {
-                        foreach ($fieldOutputKeyPosition as $fieldOutputKey => $position) {
-                            $dbItems[$id][$fieldOutputKey] = $translations[$position];
-                        }
+            foreach ($dataFields['direct'] as $field) {
+                // Get the fieldOutputKey from the cache, or calculate it
+                if (is_null($fieldOutputKeyCache[$field])) {
+                    $fieldOutputKeyCache[$field] = $fieldQueryInterpreter->getFieldOutputKey($field);
+                }
+                $fieldOutputKey = $fieldOutputKeyCache[$field];
+                // Add the text to be translated, and keep the position from where it will be retrieved
+                $contentsBySourceTargetLang[$sourceLang][$targetLang][] = $dbItems[$id][$fieldOutputKey];
+                $translationPositions[$sourceLang][$targetLang][$id][$fieldOutputKey] = $counters[$sourceLang][$targetLang];
+                $counters[$sourceLang][$targetLang]++;
+            }
+        }
+        // Translate all the contents for each pair of from/to languages
+        foreach ($contentsBySourceTargetLang as $sourceLang => $targetLangContents) {
+            foreach ($targetLangContents as $targetLang => $contents) {
+                // Get the query to send in the request, and execute against the endpoint
+                $query = $this->getQuery($provider, $sourceLang, $targetLang, $contents);
+                $response = GuzzleHelpers::requestJSON($endpointURL, $query);
+                // If the request failed, show an error and do nothing else
+                if (GeneralUtils::isError($response)) {
+                    $error = $response;
+                    $failureMessage = sprintf(
+                        $translationAPI->__('There was an error requesting data from the Provider API: %s', 'component-model'),
+                        $error->getErrorMessage()
+                    );
+                    $this->processFailure($failureMessage, [], $idsDataFields, $schemaErrors, $schemaWarnings);
+                    return;
+                }
+                $response = (array)$response;
+                // Validate if the response is the translation, or some error from the service provider
+                if ($errorMessage = $this->getErrorMessageFromResponse($provider, $response)) {
+                    $failureMessage = sprintf(
+                        $translationAPI->__('There was an error processing the response from the Provider API: %s', 'component-model'),
+                        $errorMessage
+                    );
+                    $this->processFailure($failureMessage, [], $idsDataFields, $schemaErrors, $schemaWarnings);
+                    return;
+                }
+                $translations = $this->extractTranslationsFromResponse($provider, $response);
+                // Iterate through the translations, and replace the original content in the dbItems object
+                foreach ($translationPositions[$sourceLang][$targetLang] as $id => $fieldOutputKeyPosition) {
+                    foreach ($fieldOutputKeyPosition as $fieldOutputKey => $position) {
+                        $dbItems[$id][$fieldOutputKey] = $translations[$position];
                     }
                 }
             }
