15e8ea6a59e9c7edf17b270bf88b6bf59cbcdb56
Leo <losoviz@gmail.com>
2019-11-23 17:09:19 +0800

    Typed functions

--- src/Environment.php
+++ src/Environment.php
@@ -3,12 +3,12 @@ namespace PoP\TranslateDirective;
 
 class Environment
 {
-    public static function getDefaultTranslationProvider()
+    public static function getDefaultTranslationProvider(): ?string
     {
         return $_ENV['DEFAULT_TRANSLATION_PROVIDER'];
     }
 
-    public static function useAsyncForMultiLanguageTranslation()
+    public static function useAsyncForMultiLanguageTranslation(): bool
     {
         return isset($_ENV['USE_ASYNC_FOR_MULTILANGUAGE_TRANSLATION']) ? strtolower($_ENV['USE_ASYNC_FOR_MULTILANGUAGE_TRANSLATION']) == "true" : true;
     }
