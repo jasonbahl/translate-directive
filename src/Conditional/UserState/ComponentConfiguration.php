e681d5a5a05b31e4b57765f8144bd6cdc60eb310
Leo <losoviz@gmail.com>
2020-02-24 15:23:13 +0800

    Use component configuration class

--- src/Conditional/UserState/ComponentConfiguration.php
+++ src/Conditional/UserState/ComponentConfiguration.php
@@ -1,26 +1,21 @@
 <?php
 namespace PoP\TranslateDirective\Conditional\UserState;
 
-use PoP\Hooks\Facades\HooksAPIFacade;
+use PoP\ComponentModel\AbstractComponentConfiguration;
 
-class ComponentConfiguration
+class ComponentConfiguration extends AbstractComponentConfiguration
 {
-    public const HOOK_PLACEHOLDER = __CLASS__.':%s';
     private static $userMustBeLoggedInToAccessTranslateDirective;
     public static function userMustBeLoggedInToAccessTranslateDirective(): bool
     {
-        if (is_null(self::$userMustBeLoggedInToAccessTranslateDirective)) {
-            $hooksAPI = HooksAPIFacade::getInstance();
-            $hook = sprintf(
-                self::HOOK_PLACEHOLDER,
-                'USER_MUST_BE_LOGGED_IN_TO_ACCESS_TRANSLATE_DIRECTIVE'
-            );
-            self::$userMustBeLoggedInToAccessTranslateDirective = $hooksAPI->applyFilters(
-                $hook,
-                Environment::userMustBeLoggedInToAccessTranslateDirective()
-            );
-        }
-        return self::$userMustBeLoggedInToAccessTranslateDirective;
+        $envVariable = Environment::USER_MUST_BE_LOGGED_IN_TO_ACCESS_TRANSLATE_DIRECTIVE;
+        $selfProperty = &self::$userMustBeLoggedInToAccessTranslateDirective;
+        self::maybeInitEnvironmentVariable(
+            $envVariable,
+            $selfProperty,
+            [Environment::class, 'userMustBeLoggedInToAccessTranslateDirective']
+        );
+        return (bool)$selfProperty;
     }
 }
 
--- src/Conditional/UserState/Environment.php
+++ src/Conditional/UserState/Environment.php
@@ -3,9 +3,10 @@ namespace PoP\TranslateDirective\Conditional\UserState;
 
 class Environment
 {
+    public const USER_MUST_BE_LOGGED_IN_TO_ACCESS_TRANSLATE_DIRECTIVE = 'USER_MUST_BE_LOGGED_IN_TO_ACCESS_TRANSLATE_DIRECTIVE';
     public static function userMustBeLoggedInToAccessTranslateDirective(): bool
     {
-        return isset($_ENV['USER_MUST_BE_LOGGED_IN_TO_ACCESS_TRANSLATE_DIRECTIVE']) ? strtolower($_ENV['USER_MUST_BE_LOGGED_IN_TO_ACCESS_TRANSLATE_DIRECTIVE']) == "true" : false;
+        return isset($_ENV[self::USER_MUST_BE_LOGGED_IN_TO_ACCESS_TRANSLATE_DIRECTIVE]) ? strtolower($_ENV[self::USER_MUST_BE_LOGGED_IN_TO_ACCESS_TRANSLATE_DIRECTIVE]) == "true" : false;
     }
 }
 
